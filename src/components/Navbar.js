d2566bbed4a0211c71aa4603208794d9631e65c7
Chance Reichenberg <chance.reichenberg@gmail.com>
2019-12-08 21:02:30 -0500

    updated header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,14 @@ const HeaderStyled = styled.header`
   flex-direction: column;
   align-items: center;
   padding-top: 90px;
+
+  @media screen and (min-width: 1400px) {
+    padding-top: 140px;
+  }
+
+  @media screen and (min-width: 1900px) {
+    padding-top: 200px;
+  }
   h1 {
     font-family: big-caslon-fb, serif;
 
@@ -17,6 +25,23 @@ const HeaderStyled = styled.header`
     font-style: normal;
     font-size: 28px;
     color: #3f464a;
+    margin: 0;
+
+    @media screen and (min-width: 601px) {
+      font-size: 36px;
+    }
+
+    @media screen and (min-width: 1000px) {
+      font-size: 48px;
+    }
+
+    @media screen and (min-width: 1400px) {
+      font-size: 56px;
+    }
+
+    @media screen and (min-width: 1900px) {
+      font-size: 76px;
+    }
   }
 
   ul {
@@ -25,11 +50,29 @@ const HeaderStyled = styled.header`
     font-weight: 200;
 
     font-style: normal;
+    font-size: 16px;
 
     list-style-type: none;
     text-align: center;
     padding: 0;
     margin: 0;
+
+    padding-top: 40px;
+
+    @media screen and (min-width: 1000px) {
+      font-size: 20px;
+    }
+
+    @media screen and (min-width: 1400px) {
+      padding-top: 60px;
+
+      font-size: 22px;
+    }
+
+    @media screen and (min-width: 1900px) {
+      font-size: 28px;
+      padding-top: 90px;
+    }
 
     a {
       text-decoration: none;
