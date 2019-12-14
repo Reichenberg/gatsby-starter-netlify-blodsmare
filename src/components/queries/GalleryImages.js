19bea9504d7a0c2f3453fdd24e449ddac4d59d9d
Chance Reichenberg <chance.reichenberg@gmail.com>
2019-12-14 13:46:54 -0500

    whoops wrong delete

--- src/components/queries/GalleryImages.js
+++ src/components/queries/GalleryImages.js
@@ -1,19 +1,1 @@
-import { graphql, useStaticQuery } from "gatsby";
 
-const getImages = type => {
-  const { site } = useStaticQuery(
-    graphql`
-      query SITE_METADATA_QUERY {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `
-  );
-  return site.siteMetadata;
-};
-
-export default getImages;
