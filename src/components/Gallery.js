bb6160813d1753e492962bf5741a22de0d59cc1c
Chance Reichenberg <chance.reichenberg@gmail.com>
2019-12-14 13:28:29 -0500

    added some things

---
+++ src/components/Gallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+import posed from "react-pose";
+import styled from "styled-components";
+
+const GalleryStyled = styled.div``;
+
+export default function Gallery({ type }) {
+  return <GalleryStyled></GalleryStyled>;
+}
