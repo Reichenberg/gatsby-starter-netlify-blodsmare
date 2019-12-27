b4086f3fe2a8a530ca3354df4c2033ebc81046d5
Chance Reichenberg <chance.reichenberg@gmail.com>
2019-12-27 11:21:11 -0500

    added seo components robots and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+    title: "Blodsmare | Tattoo | Illustration",
+    description: "Tattoos and Illustrations by Blodsmare.",
+    siteUrl: "https://blodsmare.com"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-robots-txt",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
